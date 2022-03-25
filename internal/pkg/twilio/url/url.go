1120d403f8ae75c15e6a11e60cb5e16d836b4895
claudio <claudio_luciano@live.com>
2022-03-25 09:28:17 +0000

    fix: Fix url

--- internal/pkg/twilio/url/url.go
+++ internal/pkg/twilio/url/url.go
@@ -15,7 +15,7 @@ func BuildWorkspaceConsoleURL(sid string) string {
 	fr := fmt.Sprintf("frameUrl=console/taskrouter/workspaces/%s/overview", sid)
 	// change / to %2F
 	fr = strings.Replace(fr, "/", "%2F", -1)
-	return fmt.Sprintf("%s/service/taskrouter/%s/taskrouter-workspace-overview/%s", base, sid, fr)
+	return fmt.Sprintf("%s/service/taskrouter/%s/taskrouter-workspace-overview?%s", base, sid, fr)
 }
 
 // BuildWorkflowConsoleURL builds the console url for a Workflow
@@ -23,7 +23,7 @@ func BuildWorkflowConsoleURL(workspaceSid, sid string) string {
 	fr := fmt.Sprintf("frameUrl=console/taskrouter/workspaces/%s/workflows/%s", workspaceSid, sid)
 	// change / to %2F
 	fr = strings.Replace(fr, "/", "%2F", -1)
-	return fmt.Sprintf("%s/service/taskrouter/%s/taskrouter-workspace-workflows/%s", base, workspaceSid, fr)
+	return fmt.Sprintf("%s/service/taskrouter/%s/taskrouter-workspace-workflows?%s", base, workspaceSid, fr)
 }
 
 // BuildTaskQueueConsoleURL builds the console url for a Taskqueue
@@ -31,7 +31,7 @@ func BuildTaskQueueConsoleURL(workspaceSid, sid string) string {
 	fr := fmt.Sprintf("frameUrl=console/taskrouter/workspaces/%s/taskqueues/%s", workspaceSid, sid)
 	// change / to %2F
 	fr = strings.Replace(fr, "/", "%2F", -1)
-	return fmt.Sprintf("%s/service/taskrouter/%s/taskrouter-workspace-taskqueues/%s", base, workspaceSid, fr)
+	return fmt.Sprintf("%s/service/taskrouter/%s/taskrouter-workspace-taskqueues?%s", base, workspaceSid, fr)
 }
 
 // BuildFlowConsoleURL builds the console url for a Flow
@@ -39,5 +39,5 @@ func BuildFlowConsoleURL(sid string) string {
 	fr := fmt.Sprintf("frameUrl=console/studio/flows/%s/Flows", sid)
 	// change / to %2F
 	fr = strings.Replace(fr, "/", "%2F", -1)
-	return fmt.Sprintf("%s/service/studio/%s/studio-flow-instance-canvas/%s", base, sid, fr)
+	return fmt.Sprintf("%s/service/studio/%s/studio-flow-instance-canvas?%s", base, sid, fr)
 }
